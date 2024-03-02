2b55278b91170831cdff9e9d0dc202fb3d51dae3
Riccardo Foffi <riccardo.foffi@protonmail.com>
2024-03-02 22:38:17 +0100

    write new Datadir function

--- src/utils.jl
+++ src/utils.jl
@@ -1,6 +1,10 @@
-# extension to use SCRATCH folder as datadir on cluster
-if haskey(ENV, "SCRATCH")
-    function DrWatson.datadir(args...)
+export Datadir
+# need custom datadir for cluster
+# but cannot overwrite the one from DrWatson
+function Datadir(args...)
+    if haskey(ENV, "SCRATCH")
         return joinpath(ENV["SCRATCH"], projectname(), args...)
+    else
+        return datadir(args...)
     end
 end
